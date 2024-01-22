0c32bb9c19adc17c09a4bb2427a13838c0b1be95
Eduarda Valério de Jesus <132959155+eduardavalerio@users.noreply.github.com>
2024-01-22 14:21:01 -0300

    phylogenetic_tree.R

--- genetic distance and phylogenetic tree/phylogenetic_tree.R
+++ genetic distance and phylogenetic tree/phylogenetic_tree.R
@@ -53,7 +53,7 @@ temp1_coi <- temp1_coi[,ncol(temp1_coi):1]
 tiff(file = "distancia_genetica_porifera_coi.tiff", width=10, heigh=8, 
      unit="in",res = 600, compression="lzw")
 par(mar = c(2,7,7,2))
-image(x = 1:44, y = 1:44, temp1, col = rev(viridisLite::viridis(100)),
+image(x = 1:44, y = 1:44, temp1_coi, col = rev(viridisLite::viridis(100)),
       xaxt = "n", yaxt = "n", xlab="",ylab="")
 axis(side = 2, at = 1:44, lab = rev(rownames(dna)), las = 2, cex.axis = .5)
 axis(side = 3, at = 1:44, lab = rownames(dna), las = 3, cex.axis = .5)
