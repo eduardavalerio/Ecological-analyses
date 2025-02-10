09e79b41f073b8ba9945371fc2511be2166da610
Eduarda Valério de Jesus <132959155+eduardavalerio@users.noreply.github.com>
2025-02-10 14:47:09 -0300

    Update phylogenetic_tree.R

--- scripts/phylogenetic_tree.R
+++ scripts/phylogenetic_tree.R
@@ -26,56 +26,44 @@ library(viridis)
 #-------------------#
 
 #------------------------------genetic distance---------------------------------------
-# A distância genética é baseada na diferença de uma sequência para outra, portanto,
-# quanto mais diferenciação entre nucleotídeos, maior será a distância genética entre
-# as espécies que estão sendo comparadas.
+# Genetic distance based in diference between sequences 
+# the greater the differentiation between nucleotides, the greater the genetic distance between
+# the species being compared.
 
-#inserir os dados -> arquivo deve estar no mesmo diretório que o script
-dna_coi <- fasta2DNAbin(file="porifera_coi_alinhado_distgen.fasta") #arquivo fasta alinhado no MEGA sem grupo externo
-dna_coi
+#data frame 
+dna_coi <- fasta2DNAbin(file="porifera_coi_alinhado_distgen.fasta") #fasta file 
 
 D_coi <- dist.dna(dna_coi, model = "k80")
-length(D_coi) #numero de comparações
+length(D_coi) #comparation numbers 
 D_coi
 
-#transformar o dataframe como matriz 
+#transform dataframe in matrix
 temp_coi <- as.data.frame(as.matrix(D_coi))
 
-#representar a distância genética em uma tabela (heatmap)
-#cores mais claras - menores diferenças; cores mais escuras - maiores diferenças 
+#represent the genetic distance into a table (heatmap) 
 table.paint(temp_coi, cleg=0, clabel.row=.6, clabel.col=.6) 
-
-
-#manipular a matrix - útil para a construção do gráfico abaixo
-#esse código converte a matriz D em uma nova matriz chamada temp e inverte a ordem das colunas dessa matriz
 temp1_coi <- t(as.matrix(D_coi))
 temp1_coi <- temp1_coi[,ncol(temp1_coi):1]
 
-#estilizar gráfico e salvar no diretório local
+#save plot
 tiff(file = "distancia_genetica_porifera_coi.tiff", width=10, heigh=8, 
      unit="in",res = 600, compression="lzw")
 par(mar = c(2,7,7,2))
 image(x = 1:44, y = 1:44, temp1_coi, col = rev(viridisLite::viridis(100)),
-      xaxt = "n", yaxt = "n", xlab="",ylab="") #mudar tamanho da imagem em x e y 1:A, sendo A o número de sequências contidas 
-axis(side = 2, at = 1:44, lab = rev(rownames(dna_coi)), las = 2, cex.axis = .5) #mudar numero de at para A 
-axis(side = 3, at = 1:44, lab = rownames(dna_coi), las = 3, cex.axis = .5) #mudar numero de at para A 
+      xaxt = "n", yaxt = "n", xlab="",ylab="") 
+axis(side = 2, at = 1:44, lab = rev(rownames(dna_coi)), las = 2, cex.axis = .5) 
+axis(side = 3, at = 1:44, lab = rownames(dna_coi), las = 3, cex.axis = .5) 
 dev.off()
 
 
-#--------------------------------árvore filogenética---------------------------------- 
+#--------------------------------phylogenetic tree---------------------------------- 
 
 ############ Maximum Likelihood-based ###############
-# O método da Máxima Verossimilhança (Maximum Likelihood, ML) é um princípio 
-# estatístico utilizado para estimar os parâmetros de um modelo estatístico. 
-# A ideia central é encontrar os valores dos parâmetros que maximizam a probabilidade
-# de observar os dados que foram realmente observados.
-# resumindo: maximizar a probabilidade de observar os dados, dado os parâmetros do modelo.
 
-#inserir os dados 
-dna1_coi <- read.phyDat("porifera_coi_alinhado_arvgen.fasta", format = "fasta")
-#arquivo fasta alinhado no MEGA com duas espécies de um grupo externo
+#data frame  
+dna1_coi <- read.phyDat("porifera_coi_alinhado_arvgen.fasta", format = "fasta") #fasta file 
 
-# escolha de modelo evolutivo de substituição de base
+# choice of evolutionary base substitution model
 mt_coi <- modelTest(dna1_coi)
 fit_coi <- as.pml(mt_coi, "BIC")
 
@@ -100,23 +88,19 @@ fit_mt_coi
 #0.2495950 0.1327952 0.1721350 0.4454748 
 
   
-fitTIM_coi <- pml_bb(dna1_coi, model="TPM3u+G(4)") #colocar o modelo fornecido
+fitTIM_coi <- pml_bb(dna1_coi, model="TPM3u+G(4)") #in model, put the output model 
 
-# bootstrap
-# O método de Bootstrap é uma técnica estatística que envolve a amostragem 
-# repetida com substituição a partir de um conjunto de dados existente. 
-# Ele é usado para estimar a variabilidade de uma estimativa estatística ou para
-# avaliar a robustez de um procedimento estatístico.
+############ Bootstrap ###############
 
 bs_coi <- bootstrap.pml(fit_mt_coi, bs=1000, optNni=TRUE,
                     control = pml.control(trace = 0))
 tree_coi <- plotBS(midpoint(fit_mt_coi$tree), bs_coi, p = 70, type="p", main="Phylogenetic tree of sponges COI")
 
 
-### salvar a árvore
+### save the tree 
 write.tree(fit_mt_coi$tree, "porifera_coi.tree")
 
-#salvar árvore como imagem tiff
+#save the tree as a tiff file 
 tiff("arvore_porifera_coi.tiff", width = 20, height = 10, units = 'in', res = 300)
 plot(tree_coi) # Make plot
 dev.off()
@@ -128,52 +112,46 @@ dev.off()
 
 #------------------------------genetic distance---------------------------------------
 
-#inserir os dados -> arquivo deve estar no mesmo diretório que o script
-dna_18s <- fasta2DNAbin(file="porifera_18s_alinhado_distgen.fasta") #arquivo fasta alinhado no MEGA sem grupo externo
+#data frame 
+dna_18s <- fasta2DNAbin(file="porifera_18s_alinhado_distgen.fasta") #fasta file 
 dna_18s
 
 D_18s <- dist.dna(dna_18s, model = "k80")
-length(D_18s) #numero de comparações
+length(D_18s) 
 D_18s
 
-#transformar o dataframe como matriz 
+#transform dataframe into a matrix
 temp_18s <- as.data.frame(as.matrix(D_18s))
 
 #representar a distância genética em uma tabela (heatmap)
-#cores mais claras - menores diferenças; cores mais escuras - maiores diferenças 
 table.paint(temp_18s, cleg=0, clabel.row=.6, clabel.col=.6) 
-
-
-#manipular a matrix - útil para a construção do gráfico abaixo
-#esse código converte a matriz D em uma nova matriz chamada temp e inverte a ordem das colunas dessa matriz
 temp1_18s <- t(as.matrix(D_18s))
 temp1_18s <- temp1_18s[,ncol(temp1_18s):1]
 
-#estilizar gráfico e salvar no diretório local
+#save the plot 
 tiff(file = "distancia_genetica_porifera_18s.tiff", width=10, heigh=8, 
      unit="in",res = 600, compression="lzw")
 par(mar = c(2,7,7,2))
 image(x = 1:51, y = 1:51, temp1_18s, col = rev(viridisLite::viridis(100)),
-      xaxt = "n", yaxt = "n", xlab="",ylab="") #mudar tamanho da imagem em x e y 1:A, sendo A o número de sequências contidas 
-axis(side = 2, at = 1:51, lab = rev(rownames(dna_18s)), las = 2, cex.axis = .5) #mudar numero de at para A 
-axis(side = 3, at = 1:51, lab = rownames(dna_18s), las = 3, cex.axis = .5) #mudar numero de at para A 
+      xaxt = "n", yaxt = "n", xlab="",ylab="") 
+axis(side = 2, at = 1:51, lab = rev(rownames(dna_18s)), las = 2, cex.axis = .5) 
+axis(side = 3, at = 1:51, lab = rownames(dna_18s), las = 3, cex.axis = .5) 
 dev.off()
 
 
-#--------------------------------árvore filogenética---------------------------------- 
+#--------------------------------phylogenetic tree---------------------------------- 
 
-#inserir os dados 
-dna1_18s <- read.phyDat("porifera_18s_alinhado_arvgen.fasta", format = "fasta")
-#arquivo fasta alinhado no MEGA com duas espécies de um grupo externo
+#data frame 
+dna1_18s <- read.phyDat("porifera_18s_alinhado_arvgen.fasta", format = "fasta") #fasta file 
 
-# escolha de modelo evolutivo de substituição de base
+# choice of evolutionary base substitution model
 mt_18s <- modelTest(dna1_18s)
 fit_18s <- as.pml(mt_18s, "BIC")
 
 fit_mt_18s <- pml_bb(mt_18s, control = pml.control(trace = 0))
 fit_mt_18s
 
-fitTIM_18s <- pml_bb(dna1_18s, model="TrN+G(4)+I") #colocar o modelo fornecido
+fitTIM_18s <- pml_bb(dna1_18s, model="TrN+G(4)+I") #output model 
 
 bs_18s <- bootstrap.pml(fit_mt_18s, bs=1000, optNni=TRUE,
                         control = pml.control(trace = 0))
@@ -181,10 +159,10 @@ bs_18s <- bootstrap.pml(fit_mt_18s, bs=1000, optNni=TRUE,
 tree_18s <- plotBS(midpoint(fit_mt_18s$tree), bs_18s, p = 70, type="p", main="Phylogenetic tree of sponges 18s")
 
 
-### salvar a árvore
+### save the tree 
 write.tree(fit_mt_18s$tree, "porifera_18s.tree")
 
-#salvar árvore como imagem tiff
+#save the tree as a tiff file 
 tiff("arvore_porifera_18s.tiff", width = 20, height = 10, units = 'in', res = 300)
 plot(tree_18s) # Make plot
 dev.off()
@@ -195,28 +173,23 @@ dev.off()
 
 #------------------------------genetic distance---------------------------------------
 
-#inserir os dados -> arquivo deve estar no mesmo diretório que o script
-dna_28s <- fasta2DNAbin(file="porifera_28s_alinhado_distgen.fasta") #arquivo fasta alinhado no MEGA sem grupo externo
+#data frame 
+dna_28s <- fasta2DNAbin(file="porifera_28s_alinhado_distgen.fasta") #fasta file 
 dna_28s
 
 D_28s <- dist.dna(dna_28s, model = "k80")
-length(D_28s) #numero de comparações
+length(D_28s) 
 D_28s
 
-#transformar o dataframe como matriz 
+#transform dataframe into a matrix
 temp_28s <- as.data.frame(as.matrix(D_28s))
 
-#representar a distância genética em uma tabela (heatmap)
-#cores mais claras - menores diferenças; cores mais escuras - maiores diferenças 
+#represent the genetic distance into a table (heatmap)
 table.paint(temp_28s, cleg=0, clabel.row=.6, clabel.col=.6) 
-
-
-#manipular a matrix - útil para a construção do gráfico abaixo
-#esse código converte a matriz D em uma nova matriz chamada temp e inverte a ordem das colunas dessa matriz
 temp1_28s <- t(as.matrix(D_28s))
 temp1_28s <- temp1_28s[,ncol(temp1_28s):1]
 
-#estilizar gráfico e salvar no diretório local
+#save the plot 
 tiff(file = "distancia_genetica_porifera_28s.tiff", width=10, heigh=8, 
      unit="in",res = 600, compression="lzw")
 par(mar = c(2,7,7,2))
@@ -225,37 +198,30 @@ image(x = 1:44, y = 1:44, temp1, col = rev(viridisLite::viridis(100)),
 axis(side = 2, at = 1:43, lab = rev(rownames(dna)), las = 2, cex.axis = .5)
 axis(side = 3, at = 1:43, lab = rownames(dna), las = 3, cex.axis = .5)
 dev.off()
-#talvez tenha que mudar at dos axis
 
-#--------------------------------árvore filogenética---------------------------------- 
+#--------------------------------phylogenetic tree---------------------------------- 
 
-#inserir os dados 
-dna1_28s <- read.phyDat("teste.fas", format = "fasta")
-#arquivo fasta alinhado no MEGA com duas espécies de um grupo externo
+#data frame  
+dna1_28s <- read.phyDat("teste.fas", format = "fasta") #fasta file 
 
-# escolha de modelo evolutivo de substituição de base
+# choice of evolutionary base substitution model
 mt_28s <- modelTest(dna1_28s)
 fit_28s <- as.pml(mt_28s, "BIC")
 
 fit_mt_28s <- pml_bb(mt_28s, control = pml.control(trace = 0))
 fit_mt_28s
 
-fitTIM_28s <- pml_bb(dna1_28s, model="TIM2+G(4)+I") #colocar o modelo fornecido
-
+fitTIM_28s <- pml_bb(dna1_28s, model="TIM2+G(4)+I") #output model
 bs_28s <- bootstrap.pml(fit_mt_28s, bs=1000, optNni=TRUE,
                         control = pml.control(trace = 0))
 
 plotBS(midpoint(fit_mt_28s$tree), bs_28s, p = 70, type="p", main="Phylogenetic tree of sponges 28s")
 
 
-### salvar a árvore
+### save the tree
 tree_28s <- write.tree(fit_mt_28s$tree, "porifera_28s.tree")
 
-#salvar árvore como imagem tiff
+#save the tree as a tiff file 
 tiff("arvore_porifera_28s.tiff", width = 20, height = 10, units = 'in', res = 300)
-plot(tree_28s) # Make plot
+plot(tree_28s) 
 dev.off()
-
-##EDIÇÃO DAS ÁRVORES 
-#apos salvar as árvores é possível edita-las usando o programa FigTree.
-# http://tree.bio.ed.ac.uk/software/figtree/
